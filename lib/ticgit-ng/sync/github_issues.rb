ca8166c86a3a27dbdd3001b98118fbadf5e1ca79
Jeff Welling <jeff.welling@gmail.com>
2011-04-18 14:52:07 -0400

    Fixed some things

--- lib/ticgit-ng/sync/github_issues.rb
+++ lib/ticgit-ng/sync/github_issues.rb
@@ -3,6 +3,9 @@ require 'octokit'
 module TicGitNG
   module Sync
     class Github_Issues
+      def initialize(options={})
+        return Github_Bugtracker.new(options)
+      end
       #list all tickets
       #called from lib/ticgit-ng/sync.rb, used to get all tickets
       def index
@@ -30,9 +33,9 @@ module TicGitNG
     class Github_Bugtracker
       def initialize(options={})
         raise "Gitub_Bugtracker.new requires {:username=>'' and either :token or :password}" unless 
-          options.include?(:username) and (options.include?( :token ) || options.include?( :password ) )
+          options.has_key?(:username) and (options.has_key?( :token ) || options.has_key?( :password ) )
 
-        if options.include? :token
+        if options.has_key? :token
           @client=Octokit::Client.new( {:login=>options[:username], :token=>options[:token]} )
         else
           @client=Octokit::Client.new( {:login=>options[:username], :password=>options[:password]} )
