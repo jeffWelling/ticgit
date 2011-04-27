a9af496411cfe4575fdddeed84b9bd7a3c1a9e0d
Jeff Welling <jeff.welling@gmail.com>
2011-04-27 00:41:23 -0400

    Removed now-superfluous Github_Issues_Bugtracker class

--- lib/ticgit-ng/sync/github_issues.rb
+++ lib/ticgit-ng/sync/github_issues.rb
@@ -61,19 +61,5 @@ module TicGitNG
       end
 
     end
-    #Class used to interface with Octokit (Github Issues)
-    class Github_Issues_Bugtracker 
-      def initialize(options={})
-        raise "Gitub_Bugtracker.new requires {:username=>'' and either :token or :password}" unless 
-          options.has_key?(:username) and (options.has_key?( :token ) || options.has_key?( :password ) )
-
-        if options.has_key? :token
-          @client=Octokit::Client.new( {:login=>options[:username], :token=>options[:token]} )
-        else
-          @client=Octokit::Client.new( {:login=>options[:username], :password=>options[:password]} )
-        end
-      end
-      attr_reader :client
-    end
   end
 end
