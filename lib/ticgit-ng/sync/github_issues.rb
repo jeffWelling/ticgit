ce3d6a5d2feb26d4a67b668fb079e425b77fe03a
Jeff Welling <jeff.welling@gmail.com>
2011-05-10 04:14:39 -0400

    More on update() in github_issues.rb

--- lib/ticgit-ng/sync/github_issues.rb
+++ lib/ticgit-ng/sync/github_issues.rb
@@ -53,11 +53,68 @@ module TicGitNG
         }
       end
       
-      #Receives a SyncableTicket and synchronizes it with the existing
-      #version of the same ticket in Github Issues
-      #When writing your own version, don't forget to use comments for any
-      #attributes which can't be updated.
-      def update ticket
+      #update() is passed a SyncableTicket, and updates the associated 
+      #Github Issues ticket with the values that have changed in SyncableTicket
+      #Do not worry about having to properly synchronize tickets, this is done 
+      #at a higher level. update() is only called to update the associated ticket
+      #with new values.
+      def update repo, ticket
+        raise "Cannot update ticket without github_id" unless ticket.github_id
+        #read ticket from github so we can tell which fields need updating
+        external_ticket= read( repo, ticket.github_id )
+
+        #check title,body
+        unless (ticket.title == external_ticket.body && ticket.body == external_ticket.body) do
+          authenticated_with :login=>@options[:username], :token=>@options[:token] do
+            issue=Issue.find(:user=>get_username(repo),:repo=>get_repo(repo),:number=>title.github_id)
+            issue.title = ticket.title
+            issue.body = ticket.body
+            issue.save
+          end
+        end
+        
+        #check state
+        unless (ticket.state == external_ticket.state)
+          authenticated_with :login=>@options[:username], :token=>@options[:token] do
+            issue=Issue.find(:user=>get_username(repo),:repo=>get_repo(repo),:number=>title.github_id)
+            if external_ticket.state == "open"
+              issue.close!
+            else
+              issue.reopen!
+            end
+          end
+        end
+        
+        #check labels
+        #FIXME the authenticated_with section looks like it can be broken out into it's own method to reduce
+        #duplicate code
+        unless (ticket.label == external_ticket.label)
+          #Add labels from ticket to external_ticket
+          unless (labels_to_update=ticket.label.collect {|i| external_ticket.label.include?(i) ? nil : i }.compact).empty?
+            authenticated_with :login=>@options[:username], :token=>@options[:token] do
+              issue=Issue.find(:user=>get_username(repo),:repo=>get_repo(repo),:number=>title.github_id)
+              #I was going to try to add all of the labels in one call, but Octopi breaks it up into one API
+              #call per label anyway so it would be a wasted effort
+              labels_to_update.each {|label| issue.add_label(label) }
+            end
+          end
+          #Remove labels on external_ticket which are not on ticket
+          unless (labels_to_update=external_ticket.label.collect {|i| ticket.label.include?(i) ? nil : i }.compact).empty?
+            authenticated_with :login=>@options[:username], :token=>@options[:token] do
+              issue=Issue.find(:user=>get_username(repo),:repo=>get_repo(repo),:number=>title.github_id)
+              labels_to_update.each {|label| issue.remove_label(label) }
+            end
+        end
+
+        #check comment_body
+        
+        x=eval("fubar")
+
+        #ticgit_ticket.title="fubar"
+        #ticgit_ticket.body="fubar fubar fubar"
+        #ticket=ticgit_ticket
+        #
+        #
       end
       
       def destroy
