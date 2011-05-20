97080783aebcfdae860d6c1fb10e06c04b5bce64
Jeff Welling <Jeff.Welling@Gmail.com>
2011-05-20 15:16:36 -0700

    Tiny spelling fix in comment

--- lib/ticgit-ng/base.rb
+++ lib/ticgit-ng/base.rb
@@ -30,7 +30,7 @@ module TicGitNG
       @state = File.expand_path(File.join(@tic_dir, proj, 'state'))
 
       if File.file?(@state)
-        #populate @last_ticket, @current_ticket
+        #populate @last_tickets, @current_ticket
         load_state
       else
         reset_ticgitng
