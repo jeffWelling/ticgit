2405d96dcf9db0dc6e04e95e733ddab9901dea1b
Jeff Welling <jeff.welling@gmail.com>
2012-08-21 15:17:47 -0700

    Added more specs for syncing

--- spec/sync_spec.rb
+++ spec/sync_spec.rb
@@ -16,7 +16,42 @@ describe TicGitNG do
     Dir.glob(File.expand_path("/tmp/ticgit-ng-*")).each {|file_name| FileUtils.rm_r(file_name, {:force=>true,:secure=>true}) }
   end
 
+  it "Should be able to register sync modules"
+  it "Should get the constant name of the sync module related to 'remote'"
+  it "SyncableTicket should have a list of usable attributes"
+  it "SyncableTicket should have a method to detect nonstandard attributes in self"
+  it "SyncableTicket should be able to detect wether or not the comments contain attribute updates"
+  it "SyncableTicket should be able to update attributes if attr updates exist in comments"
+  it "SyncableTicket should have a list of imutable attributes"
+  it "SyncableTicket should be able to appropriately update all legal values via attr updates in comments"
+  it "SyncableTicket should not allow attribute updates in comments to update imutable attributes"
+  it "SyncableTicket should allow access to all of the internal attributes"
+  it "Should be able to extract a bug tracker from a source string eg. 'github:jeffWelling/ticgit'"
+  it "Should be able to extract a repo from a source string eg. 'github:jeffWelling/ticgit'"
+  it "Should fail gracefully if a bug tracker is used when a requisite gem is not installed"
+
+
+  #for each external bug tracker
+  it "Should be able to get auth info for the bug tracker"
+  it "Should be able to merge in tickets from github using a test fake repo"
+  it "Should be able to merge in tickets locally and push the result back up"
+  it "Should be able to consult the user when a merge conflict arises"
+  it "Should be able to push resolved merge conflicts back upstream"
+  it "Should present a simple printout indicating success or failure, with a short summary"
+
+  #for each attribute
+  it "Should be able to update existing tickets with remote changes"
+  it "Should be able to update remote tickets with local changes"
+
+
+
   it "Should merge in tickets from a remote source" do
+    #Add a secondary TicGit-ng source
+    #add tickets to the remote source
+    #add a remote for the secondary TicGit-ng source
+    #pull in the tickets from the remote source
+    #check that the tickets pulled are the ones in the secondary source
+    #check that the tickets in source 1 and source 2 are the same
     Dir.chdir(File.expand_path( tmp_dir=Dir.mktmpdir('ticgit-ng-gitdir1-') )) do
       #prep, get temp dirs, init git2
       @ticgitng.ticket_new('my new ticket')
@@ -42,6 +77,10 @@ describe TicGitNG do
   end
 
   it "should be able to sync with origin" do
+    #create tickets in first repo
+    #init second repo
+    #sync with second repo
+    ### - TicGit-ng should check for existing upstrea repo and pull if exists
     Dir.chdir(File.expand_path( tmp_dir=Dir.mktmpdir('ticgit-ng-gitdir1-') )) do
       #prep, get temp dirs, init git2
 
@@ -80,6 +119,10 @@ describe TicGitNG do
   end
 
   it "should be able to sync with other repos" do
+    #init first repo
+    #init second third repo
+    #create tickets in second and third repos
+    #sync second and third repos with first repo
     Dir.chdir(File.expand_path( tmp_dir=Dir.mktmpdir('ticgit-ng-gitdir1-') )) do
       #prep, get temp dirs, init git2
 
@@ -146,6 +189,14 @@ describe TicGitNG do
     end
   end
   it "Use the 'ticgit' branch if 'ticgit-ng' isn't available (legacy support)" do
+    #create first repo with 'ticgit' branch
+    #clone first to second repo
+    #second repo should have used the 'ticgit' repo
+    #
+    #new first repo
+    #create 'ticgit-ng' branch
+    #clone new first repo to second repo
+    #second repo should have used the 'ticgit' branch
     Dir.chdir(File.expand_path( tmp_dir=Dir.mktmpdir('ticgit-ng-gitdir1-') )) do
       #Because there is no 'ticgit' or 'ticgit-ng' branch
       #this will create a new ticket in 'ticgit-ng'
@@ -182,4 +233,6 @@ describe TicGitNG do
       # end
     end
   end
+  it "should fail gracefully if attempting to sync with upstream repo when 'origin' doesn't exist"
+  it "should be able to predict merge conflicts using a whitelist approach"
 end
